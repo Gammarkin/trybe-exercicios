e33c3e529282f3f41f0bc5e51d29fc9f48a6d31c
Gammarkin <marcosrbnsf@gmail.com>
2022-02-04 18:02:15 -0300

    new arqs

---
+++ fundamentos/Bloco 04: Introdução à JavaScript e Lógica de Programação/4.2-javascript-array-e-loop-for/loopfor.js
@@ -0,0 +1,12 @@
+/*let soma = 0;
+for (let i = 0; i <= 50; i++) {
+  console.log(i);
+  soma = soma + i;
+}
+console.log("a soma dos termos é" + soma);*/
+let sum;
+for (let i = 1; i > 150; i++) {
+  if (i % 3 == 0) {
+    sum + i;
+  }
+}
