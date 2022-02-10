ed8a53414f4829093729524b9fd20d65bd0874a3
Gammarkin <marcosrbnsf@gmail.com>
2022-02-10 16:08:30 -0300

    adding ex

---
+++ fundamentos/Bloco 05: JavaScript: DOM, Eventos e Web Storage/dia-2-javascript-trabalhando-com-elementos/ex1/script.js
@@ -0,0 +1,16 @@
+//1
+document.getElementById("elementoOndeVoceEsta");
+//2
+document.getElementById("elementoOndeVoceEsta").parentNode.style.color = "blue";
+//3
+document.getElementById("primeiroFilhoDoFilho").innerText = "eitakkk";
+//4
+document.getElementById("pai").firstElementChild;
+//5
+document.getElementById("elementoOndeVoceEsta").parentElement.firstElementChild;
+//6
+document.getElementById("elementoOndeVoceEsta").nextSibling;
+//7
+document.getElementById("elementoOndeVoceEsta").nextElementSibling;
+//8
+document.getElementById("pai").lastElementChild.previousElementSibling;
