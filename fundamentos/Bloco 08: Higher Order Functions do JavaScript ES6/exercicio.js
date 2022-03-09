4e96ce29ba4d3d6548cb1973195aff8b7a4f41ad
Markin <marcosrbnsf@gmail.com>
2022-03-09 16:09:41 -0300

    made ex

--- fundamentos/Bloco 08: Higher Order Functions do JavaScript ES6/exercicio.js
+++ fundamentos/Bloco 08: Higher Order Functions do JavaScript ES6/exercicio.js
@@ -13,4 +13,33 @@ const newEmployees = (func) => {
     return employees;
 };
 
-console.log(newEmployees(nameAndEmail));
+// console.log(newEmployees(nameAndEmail));
+
+const randomNumber = () => Math.round(Math.random() * 100);
+const didItWin = (fun) => fun();
+const checkIfWon = (num) => {
+    const result = didItWin(randomNumber);
+    return (num === result) ? 'Parabéns, você ganhou!' : 'Tente novamente!';
+}
+
+// console.log(checkIfWon(8));
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const answerScore = (rightAnswer, studentAnswer) => {
+    if (rightAnswer === studentAnswer) return 1;
+    if (studentAnswer === 'N.A') return 0;
+    return -0.5;
+};
+
+const countPoints = (rightAnswers, studentAnswers, func) => {
+    let score = 0;
+    for (let index = 0; index < studentAnswers.length; index += 1) {
+        const returnFunc = func(rightAnswers[index], studentAnswers[index]);
+        score += returnFunc;
+    }
+    return `Nota: ${score}`;
+};
+
+// console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, answerScore));
