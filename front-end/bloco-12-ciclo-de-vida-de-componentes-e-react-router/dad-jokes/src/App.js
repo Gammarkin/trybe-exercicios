ec30fad6cbb8327800c658a86d4124c80b4a84e0
Markin <marcosrbnsf@gmail.com>
2022-04-14 20:23:02 -0300

    adding some bla

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dad-jokes/src/App.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dad-jokes/src/App.js
@@ -1,46 +1,19 @@
 // App.js
 import React, { Component }from 'react';
+import ComponentePai from './components/component';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-        characters: [],
-    };
-  }
-
-  fetchCharacters = () => {
-    fetch('https://rickandmortyapi.com/api/character')
-    .then(response => response.json())
-    .then(data => {
-      this.setState({characters: data.results})
-    })
-  }
-
-  componentDidMount() {
-    this.fetchCharacters();
-  }
-
   render() {
-    const { characters } = this.state;
     return (
-      <div className="App">
-        <h1>
-          Ricky and Morty Characters:
-        </h1>
-        <div className="body">
-          {characters.map(({ name, image }) => {
-            return (
-              <div className="container" key={name}>
-                <h3>{name}</h3>
-                <img src={image} alt={name}/>
-              </div>
-            )
-          })}
-        </div>
+      <div className='main'>
+        <ComponentePai>
+          <p>SUPIMPA</p>
+          <h1>BACANA</h1>
+          <span>INCRÍVEL</span>
+        </ComponentePai>
       </div>
-    );
+    )
   }
 }
 
