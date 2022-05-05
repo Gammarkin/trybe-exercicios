31cd157c02519050c52a6536382c786b8e87c836
Markin <marcosrbnsf@gmail.com>
2022-05-05 17:05:38 -0300

    made plain js test exercise

---
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/service.js
@@ -0,0 +1,25 @@
+const randomNumberBetween0and100 = () =>
+    Math.floor(Math.random() * (100 - 0));
+
+const capsLock = (str) => str.toUpperCase();
+
+const firstLetter = (str) => str[0];
+
+const concat = (str1, str2) => str1.concat(str2);
+
+const fetchDog = async () => {
+    const URL = "https://dog.ceo/api/breeds/image/random";
+    const response = await fetch(URL);
+    const data = response.ok
+        ? Promise.resolve(response.json())
+        : Promise.reject(response.json());
+    return data;
+}
+
+module.exports = {
+    randomNumberBetween0and100,
+    capsLock,
+    firstLetter,
+    concat,
+    fetchDog,
+}
