dc64e78c2c90cae46ef24804d2fe26cbac1a1f75
Markin <marcosrbnsf@gmail.com>
2022-07-20 13:49:01 -0300

    made exercise 3

---
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/02-01/ex3_inner_join.sql
@@ -0,0 +1,12 @@
+-- id do cliente, nome e email, 
+-- id do endereço e o nome da rua
+-- customer e address
+-- 100 clientes, ordenados pelo nome em ordem decrescente
+
+USE sakila;
+
+SELECT cust.customer_id, cust.first_name, cust.email, cust.address_id,
+ad.address
+FROM customer `cust`
+INNER JOIN address `ad`
+ON ad.address_id = cust.address_id
