9c285226a889bb6ec4e2bd8a82a3939d472f6739
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-26 17:58:04 -0300

    adding exercise 6

---
+++ back-end/bloco-22-node/exercicios/velocidade.js
@@ -0,0 +1,42 @@
+// v = d / t
+
+const calcualteVelocity = () => {
+  const QUESTIONS = [
+    'Qual a distância percorrida? (em kilometros)',
+    'Qual o tempo gasto? (em horas)',
+  ];
+
+  const askQuestion = (i = 0) => {
+    process.stdout.write(`\n${QUESTIONS[i]} \n`);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write('   > ');
+  };
+
+  askQuestion();
+
+  const answers = [];
+
+  process.stdin.on('data', (data = 0) => {
+    if (isNaN(Number(data)) || data === '\n') {
+      process.stdout.write('Digite um número\n');
+    } else {
+      answers.push(Number(data));
+      if (answers.length < QUESTIONS.length) {
+        askQuestion(answers.length);
+      } else {
+        const [distance, time] = answers;
+        const velocity = distance / time;
+
+        process.stdout.write(
+          `\n\nA velocidade é de ${velocity.toFixed(2)} km/h.\n`
+        );
+        process.exit();
+      }
+    }
+  });
+};
+
+calcualteVelocity();
+
+module.exports = calcualteVelocity;
