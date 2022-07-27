789fbffa81ab8e097ec426d500a9944629c06fef
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-27 17:43:24 -0300

    finished exercise 4

--- back-end/bloco-22-node/node-fluxo-async/exercise4.js
+++ back-end/bloco-22-node/node-fluxo-async/exercise4.js
@@ -28,10 +28,39 @@ const removeChars = async (...charId) => {
   await fs.writeFile('./simpsons.json', JSON.stringify(filteredChars));
 };
 
+const createNewReducedFile = async () => {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+  const charsArr = JSON.parse(fileContent);
+  const MAX_LENGHT_OF_CHARS = 4;
+
+  const filteredChars = charsArr.slice(0, MAX_LENGHT_OF_CHARS);
+  await fs.writeFile('./simpsonFamily.json', JSON.stringify(filteredChars));
+};
+
+const addNelsonToFamily = async () => {
+  const fileContent = await fs.readFile('./simpsonFamily.json', 'utf-8');
+  const charsArr = JSON.parse(fileContent);
+
+  charsArr.push({id: '5', name: 'Nelson Muntz', yimsw: '1'});
+  await fs.writeFile('./simpsonFamily.json', JSON.stringify(charsArr));
+};
+
+const changeNelsonForMaggie = async () => {
+  const fileContent = await fs.readFile('./simpsonFamily.json', 'utf-8');
+  const charsArr = JSON.parse(fileContent);
+  charsArr[4].name = 'Maggie Simpson';
+  charsArr[4].yimsw = '0';
+
+  await fs.writeFile('./simpsonFamily.json', JSON.stringify(charsArr));
+};
+
 const main = () => {
-  // readChars();
-  // filterCharsById(1);
-  // removeChars(10, 6);
+  readChars();
+  filterCharsById(1);
+  removeChars(10, 6);
+  createNewReducedFile();
+  addNelsonToFamily();
+  changeNelsonForMaggie();
 };
 
 main();
