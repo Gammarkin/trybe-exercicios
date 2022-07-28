c1d7755f2aaec51264068227b13388bd6aea464e
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-28 18:09:25 -0300

    finished exercise 3

--- back-end/bloco-22-node/testes-back-end/exercicios/tests/numberType.js
+++ back-end/bloco-22-node/testes-back-end/exercicios/tests/numberType.js
@@ -5,10 +5,12 @@ const numberType = require('../funcs/numberType');
 const EXPECTED_NEUTRAL = 'neutro';
 const EXPECTED_POSITIVE = 'positivo';
 const EXPECTED_NEGATIVE = 'negativo';
+const EXPECTED_NaN = 'o valor deve ser um número';
 
 const NEUTRAL_EXAMPLE = 0;
 const POSITIVE_EXAMPLE = 5;
 const NEGATIVE_EXAMPLE = -5;
+const NaN_EXAMPLE = 'oi :)';
 
 describe('retorna o tipo de numero', () => {
   describe('quando o numero for 0', () => {
@@ -26,6 +28,12 @@ describe('retorna o tipo de numero', () => {
   describe('quando o numero for maior que zero', () => {
     it('espera que a resposta seja positivo', () => {
       expect(numberType(POSITIVE_EXAMPLE)).to.equal(EXPECTED_POSITIVE);
+    });
+  });
+
+  describe('quando não for passado um número', () => {
+    it('espera que a resposta seja "o valor deve ser um número"', () => {
+      expect(numberType(NaN_EXAMPLE)).to.equal(EXPECTED_NaN);
     });
   });
 });
