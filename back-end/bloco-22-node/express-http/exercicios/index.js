8d15ace3d1a2c67784c0372ef7d7f072e2cc71e4
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-29 19:45:38 -0300

    finished exercise 3

--- back-end/bloco-22-node/express-http/exercicios/index.js
+++ back-end/bloco-22-node/express-http/exercicios/index.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/ping', (_req, res) => {
   res.status(200).json({message: 'pong'});
+});
+
+app.post('/hello', (req, res) => {
+  const {name} = req.body;
+
+  res.status(200).json({message: `Hello, ${name}!`});
+});
+
+app.post('/greetings', (req, res) => {
+  const {name, age} = req.body;
+
+  if (Number(age) <= 17) {
+    return res.status(401).json({message: 'Unauthorized'});
+  }
+
+  return res.status(200).json({message: `Hello, ${name}!`});
 });
 
 app.listen(8080, () => {
