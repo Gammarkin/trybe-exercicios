7fd7dea4a513957c6407a0b61152dff77f90a069
Gammarkin <92131987+Gammarkin@users.noreply.github.com>
2022-07-29 18:35:01 -0300

    finished fixar

--- back-end/bloco-22-node/express-http/criando-express/fixar1.js
+++ back-end/bloco-22-node/express-http/criando-express/fixar1.js
@@ -1,55 +1,70 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 
 const recipes = [
-  {id: 1, name: 'Lasanha', price: 40.0, waitTime: 30},
-  {id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25},
-  {id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25},
+  {id: 1, name: 'Lasanha', preco: 40.0, tempoDePreparo: 30},
+  {id: 2, name: 'Macarrão a Bolonhesa', preco: 35.0, tempoDePreparo: 25},
+  {id: 3, name: 'Macarrão com molho branco', preco: 35.0, tempoDePreparo: 25},
 ];
 
-const drinks = [
-  {id: 1, name: 'Refrigerante Lata', price: 5.0},
-  {id: 2, name: 'Refrigerante 600ml', price: 8.0},
-  {id: 3, name: 'Suco 300ml', price: 4.0},
-  {id: 4, name: 'Suco 1l', price: 10.0},
-  {id: 5, name: 'Cerveja Lata', price: 4.5},
-  {id: 6, name: 'Água Mineral 500 ml', price: 5.0},
-];
-
-app.get('/recipes', function (_req, res) {
-  res.json(recipes);
+app.get('/recipes', function (req, res) {
+  res.status(200).json(recipes);
 });
 
-app.get('/drinks', (_req, res) => {
-  res.json(drinks);
-});
-
-app.get('/drinks/:id', (req, res) => {
-  const {id} = req.params;
-  const selectedDrink = drinks.find((drink) => drink.id === Number(id));
-
-  if (!selectedDrink)
-    return res.status(404).json({message: 'Drink not found!'});
-
-  res.status(200).json(selectedDrink);
-});
-
-app.get('/recipes/:id', (req, res) => {
-  const {id} = req.params;
-  const selectedRecipe = recipes.find((recipe) => recipe.id === Number(id));
-
-  if (!selectedRecipe)
-    return res.status(404).json({message: 'Recipe not found!'});
-
-  res.status(200).json(selectedRecipe);
-});
-
-app.get('/recipes/search', function (req, res) {
-  const {name} = req.query;
-  const filteredRecipes = recipes.filter((r) => r.name.includes(name));
+app.get('/recipes/pesquisar', function (req, res) {
+  const {name, maxPrice} = req.query;
+  const filteredRecipes = recipes.filter(
+    (r) => r.name.includes(name) && r.preco < Number(maxPrice)
+  );
   res.status(200).json(filteredRecipes);
 });
 
-app.listen('8080', () => {
-  console.log('listening in port 8080.');
+app.get('/recipes/:id', function (req, res) {
+  const {id} = req.params;
+  const recipe = recipes.find((r) => r.id === Number(id));
+  if (!recipe) return res.status(404).json({message: 'Recipe not found!'});
+
+  res.status(200).json(recipe);
+});
+
+app.post('/recipes', function (req, res) {
+  const {id, name, price} = req.body;
+  recipes.push({id, name, price});
+  res.status(201).json({message: 'Recipe created successfully!'});
+});
+
+app.put('/recipes/:id', function (req, res) {
+  const {id} = req.params;
+  const {name, price} = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === Number(id));
+
+  if (recipeIndex === -1)
+    return res.status(404).json({message: 'Recipe not found!'});
+
+  recipes[recipeIndex] = {...recipes[recipeIndex], name, price};
+
+  res.status(204).end();
+});
+
+app.delete('/recipes/:id', function (req, res) {
+  const {id} = req.params;
+  const recipeIndex = recipes.findIndex((r) => r.id === Number(id));
+
+  if (recipeIndex === -1)
+    return res.status(404).json({message: 'Recipe not found!'});
+
+  recipes.splice(recipeIndex, 1);
+
+  res.status(204).end();
+});
+
+app.all('*', function (req, res) {
+  return res.status(404).json({message: `Rota '${req.path}' não existe!`});
+});
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
 });
